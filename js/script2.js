1b54c43a7fce5b9061c29de079b0715c16614463
Unive200 <zevseychev0@gmail.com>
2023-08-02 09:13:12 +0300

    master

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,16 @@
+function gotoLink (link) {
+  console.log (link.value)
+  window.open(link.value) // Открыть в новом окне
+};
+
+
+
+
+
+
 if (confirm("Перейти на Music Store?")) {
-  location.href = "https://musicstore.com"; // перенаправляет браузер на другой URL
-}
-else {
-  alert("Отказываетесь? ");
-}
+   location.href = "https://musicstore.com"; // перенаправляет браузер на другой URL
+ }
+ //else {
+  // location.href = "https://muztorg.ru";
+ //}
